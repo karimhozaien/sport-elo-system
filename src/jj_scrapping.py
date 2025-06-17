41daaaf3a0e1951d3fbce5037a14a9e117d4a522
karimhozaien <karimhozaien@gmail.com>
2025-06-17 01:00:11 -0400

    getting each fighters personal page

--- src/jj_scrapping.py
+++ src/jj_scrapping.py
@@ -1,16 +1,24 @@
+from curses.ascii import islower
+
 import requests
 from bs4 import BeautifulSoup
-import time
 
 url = 'https://www.bjjheroes.com/'
 url_az = url + '/a-z-bjj-fighters-list'
+url_fighters = url + 'bjj-fighters/'
 
 response = requests.get(url_az)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-tables = soup.find_all('td')
+# Find all first and last names
+first_names = soup.find_all('td', class_='column-1')
+last_names = soup.find_all('td', class_='column-2')
 
-for table in tables:
-    names = table.find('a')
-    if names:
-        print(names.get_text(strip=True))
+# Iterate over the names and print them
+if first_names and last_names:
+    for first, last in zip(first_names, last_names):
+        print(first.get_text(strip=True))
+        print(last.get_text(strip=True))
+
+        url_name = url_fighters + first.get_text(strip=True) +'-' + last.get_text(strip=True)
+        print(url_name)
